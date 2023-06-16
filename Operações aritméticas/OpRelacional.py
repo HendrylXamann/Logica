8a79563875eb3b5a0869d285017c8da2a6bce83a
Hendryl <75086675+HendrylXamann@users.noreply.github.com>
2023-06-16 12:54:15 -0300

    att2

--- Operações aritméticas/OpRelacional.py
+++ Operações aritméticas/OpRelacional.py
@@ -1,17 +1,30 @@
 #Segue uma demonstração básica de operações relacional no python visando demonstrar meus conhecimentos no tema;
-#Versão extensa/arcaica 1
+#Versão extensa/arcaica de Maior ou igual que (1):
 var11 = 7
 var22 = 4
-rel = var11 > var22
+rel = var11 >= var22
 print(rel)
-#Versão otimizada/prática 2
+#Versão otimizada/prática de Maior ou igual que (1):
 var11, var22 = 7,4
-print(var11 > var22)
-#Versão extensa/arcaica 2
-var11 = 7
-var22 = 4
-rel = var11 < var22
+print(var11 >= var22,"\n--------")
+#Versão extensa/arcaica de Menor ou igual que (2):
+var111 = 7
+var222 = 4
+rel = var111 <= var222
 print(rel)
-#Versão otimizada/prática 2
-var11, var22 = 7,4
-print(var11 < var22)
+#Versão otimizada/prática de Menor ou igual que (2):
+var111, var222 = 7,4
+print(var111 <= var222,"\n--------")
+#Versão extensa/arcaica de não igual que (3):
+var1111 = 7
+var2222 = 4
+rel = var1111 != var2222
+print(rel)
+#Versão otimizada/prática de não igual que (3):
+var110, var223, = 7,4
+print(var110 != var223,"\n--------")
+#Versão otimizada/prática de igual a (4):
+var, var9 = 7,4
+other1 = 7
+print(var == other1)
+print(var9 == other1)
