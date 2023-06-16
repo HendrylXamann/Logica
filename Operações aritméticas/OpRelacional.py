fc9edbb1d489f7a93cb1202db47e95882be7a51a
Hendryl <75086675+HendrylXamann@users.noreply.github.com>
2023-06-16 12:34:21 -0300

    rel

---
+++ Operações aritméticas/OpRelacional.py
@@ -0,0 +1,9 @@
+#Segue uma demonstração básica de operações relacional no python visando demonstrar meus conhecimentos no tema;
+#Versão extensa/arcaica
+var11 = 7
+var22 = 4
+rel = var11 > var22
+print(rel)
+#Versão otimizada/prática
+var11, var22 = 7,4
+print(var11 > var22)
