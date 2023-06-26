c00167af1522f912dd40e1153224d6be8a663371
Hendryl <75086675+HendrylXamann@users.noreply.github.com>
2023-06-26 11:56:04 -0300

    New

---
+++ VARIÁVEIS/VariavelWithPython02.py
@@ -0,0 +1,12 @@
+#Aqui é um trecho de código que envolve o variáveis do tipo 
+#string e int:
+print('Iae')
+print('Qual seu nome?') 
+nome = str(input())
+print('Satisfação Sr(a) ' + nome)
+lenn = len(nome)
+print('Você já reparou que eu nome tem ' + str(int(lenn)) + ' letras?')
+#print(lenn)
+print('Mas mudando de assunto, fala ae, qual sua idade?') 
+idade = input()
+print('Massa, ano que vem você terá ' + str(int(idade) + 1) + ' anos.')
