f53b6c213a3445a85b4ddbcf1c4e5d52ef85e49b
Hendryl <75086675+HendrylXamann@users.noreply.github.com>
2023-07-03 19:52:46 -0300

    Update VariavelWithPython00.py

--- VARIÁVEIS/VariavelWithPython00.py
+++ VARIÁVEIS/VariavelWithPython00.py
@@ -1,8 +1,8 @@
 # Aqui está apenas um trecho de código utilizando variáveis cujo os valores são do tipo string, float e também uma tupla;
-variAvel1 = "Python não é somente KamelKaze mas Case Sensitive também."
+variAvel1 = "Python é Case Sensitive, logo variavel e Variavel serão diferentes."
 print(variAvel1)
 
-VARIAVEL2 = "Normalmente fazemos separações no corpo da variável usando underline."
+VARIAVEL2 = "Normalmente fazemos separações no corpo da variável usando underline." #Mas não quer dizer que não posso declarar de várias formas
 print(VARIAVEL2)
 
 variavel_3 = "Podemos colocar valores numéricos separados por ponto tranquilamente:"
